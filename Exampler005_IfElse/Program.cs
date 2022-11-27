4e3bb6c82d059e87e8c57d6fd24f391adbfbd518
Serhii <tsymbal0511@gmail.com>
2022-11-27 21:00:59 +0300

    синтаксис с условиями

---
+++ Exampler005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите свое имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
