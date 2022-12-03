a8f8ae9effe3484954267818b0ce6a8c48b214ba
Serhii <tsymbal0511@gmail.com>
2022-12-03 17:11:31 +0300

    add les2

---
+++ Lessons2/Lessons2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99]
+//и показывет найбольшую цифру числа 78 -> 8 12 -> 2
+
+Random random = new Random();
+
+int randomNumber = random.Next(10,100);
+
+int lastDigit = randomNumber % 10;
+int firstDigit = randomNumber / 10 % 10;
+
+Console.WriteLine(randomNumber);
+
+if (lastDigit > firstDigit)
+{
+    Console.WriteLine(lastDigit);
+}
+else
+{
+    Console.WriteLine(firstDigit);
+}
+
+//11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46 782 -> 72 918 -> 98
+
