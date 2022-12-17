f5aeb1a774d897f6cb4fc32afbc94ade1500b6b0
Serhii <tsymbal0511@gmail.com>
2022-12-17 16:42:29 +0300

    Webinar 6

---
+++ Lessons6/Program.cs
@@ -0,0 +1,160 @@
+﻿//Задача 39: Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+//[1 2 3 4 5] -> [5 4 3 2 1]
+//[6 7 3 6] -> [6 3 7 6]
+// int a = 5;
+// int b = 6;
+// int tmp = a;
+// a = b;
+// b = tmp;
+
+/*
+int[] array = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] tmp = new int[array.Length];
+Array.Copy(array, tmp, array.Length);
+Reverse(array);
+PrintArray(array);
+
+PrintArray(tmp);
+
+int[] reversed = Reverse2(array);
+PrintArray(reversed);
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine(string.Join(",", array));
+}
+
+void Reverse(int[] array)
+{  
+    int tmp;
+    for (var i = 0; i < array.Length / 2; i++)
+    {
+        tmp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = tmp;
+    }
+}
+
+int[] Reverse2(int[] array)
+{  
+    int[] reversed = new int[array.Length];
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        reversed[i] = array[array.Length - 1 - i];
+    }
+
+    return reversed;
+}
+
+
+//Задача 40: Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать
+//треугольник с сторонами такой длины.
+//Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+//Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное. 45 -> 101101
+//3 -> 11, 2 -> 10
+
+//Задача 44: Не используя рекурсию, выведите первые
+//N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и
+//1. Если N = 5 -> 0 1 1 2 3 Если N = 3 -> 0 1 1
+//Если N = 7 -> 0 1 1 2 3 5 8
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+
+GetFalseorTrue(number1, number2, number3);
+
+Console.WriteLine(GetFalseorTrue(number1, number2, number3));
+
+bool GetFalseorTrue(int a, int b, int c)
+{
+    if ( a < (b+c) & b < (a+c) & c < (b+a) )
+    {
+        return true;
+    }
+    return false;
+
+//string number = Convert.toString();
+
+}
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Get2numberis10(number));
+
+string Get2numberis10(int number)
+{
+    string text = "";
+
+    for (int i = 0; 0 < number; i++)
+    {
+        text = number%2 + text;
+        number /=2;
+    }
+    return text;
+}
+
+
+
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(string.Join(",", GetArrayFibonachi(number)));
+
+int[] GetArrayFibonachi(int number)
+{
+    int[] array = new int[number];
+    if (number > 0) array[0] = 0;
+    if (number > 1) array[1] = 1;
+
+    for (int i = 2; i < array.Length; i++)
+    {
+        array[i] = (array[i - 1]) + (array[i - 2]);
+    }
+
+    return array;
+
+}
+void PrintArray(int[] array)
+{
+    Console.WriteLine(string.Join(",", array));
+}
+*/
+
+int number = int.Parse(Console.ReadLine());
+int[] array = new int[number];
+
+Console.WriteLine(string.Join(",", GetRandomNumber(array)));
+Console.WriteLine(string.Join(",", GetArrayCopy(array)));
+
+int[] GetArrayCopy(int[] array)
+{
+    int[] arraycopy = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arraycopy[i] = array[i];
+    }
+    return arraycopy;
+}
+
+int[] GetRandomNumber(int[] array)
+{
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(1, 15);
+    }
+    return array;
+}
+
+
+
+
+
+
+
+
+
+
+
